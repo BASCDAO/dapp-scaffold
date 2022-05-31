4f6c1141d4e4134c8350dbb38f8321587ed01b9c
BASCDAO <daobasc@gmail.com>
2022-05-31 14:33:55 -0500

    ok

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,19 +1,14 @@
-import { FC, useEffect, useState } from 'react';
+import * as React from 'react';
 import ReactMapGL, { NavigationControl } from 'react-map-gl';
-import mapboxgl from 'mapbox-gl';
 
-export const BasicsView: FC = ({ }) => {
-
-/*const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN || '';*/
-const MAPBOX_TOKEN ="pk.eyJ1IjoiZXh4ZW1wdCIsImEiOiJjbDI4Nm85ZW8wNmcwM2RvMXNtYnVncWN5In0.IvpgiphMkVw3Nlw8ynLoSQ"
+const MAPBOX_TOKEN = "pk.eyJ1IjoiZXh4ZW1wdCIsImEiOiJjbDI4Nm85ZW8wNmcwM2RvMXNtYnVncWN5In0.IvpgiphMkVw3Nlw8ynLoSQ" || '';
 const initialState = {
     viewport: {
-      latitude: 8.950357,
-      longitude: -20.604661,
-      position: "absolute",
-      width: "100vw",
-      height: "100vh",
-      zoom: 2.23,
+        height: 400,
+        latitude: 37.776021,
+        longitude: -122.4171949,
+        width: 400,
+        zoom: 14,
     },
 };
 type State = typeof initialState;
@@ -53,7 +48,6 @@ export default class Map extends React.Component<{}, State> {
             <ReactMapGL
                 {...viewport}
                 mapboxApiAccessToken={MAPBOX_TOKEN}
-                mapStyle="mapbox://styles/exxempt/cl3klweqn001m14p0qfc9jpcr"
                 onViewportChange={(v: Viewport) => this.updateViewport(v)}
             >
                 <div style={{ position: 'absolute', right: 30, bottom: 30 }}>
@@ -63,4 +57,3 @@ export default class Map extends React.Component<{}, State> {
         );
     }
 }
-};
